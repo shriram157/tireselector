3c823eab219bc5b5815c5d5880fbb3487404b4b5
Yang Ray <ray_yang@toyota.ca>
2018-12-24 15:11:25 +0000

    adding new node

--- tireSelector_Node/myRouter/index.js
+++ tireSelector_Node/myRouter/index.js
@@ -2,5 +2,6 @@
 'use strict';
 module.exports = (app, server) => {
 	app.use('/node', require('./myRoutes/myNode')());
+	app.use('/appdata', require('./myRoutes/env')());
 
 };
---
+++ tireSelector_Node/myRouter/myRoutes/env.js
@@ -0,0 +1,38 @@
+/*eslint no-console: 0, no-unused-vars: 0, no-shadow: 0, new-cap: 0*/
+/*eslint-env node, es6 */
+'use strict';
+
+var xsenv = require('@sap/xsenv');
+var express = require('express');
+
+module.exports = function() {
+	var app = express.Router();
+
+	app.get('/whoAmI', (req, res) => {
+		var userContext = req.authInfo;
+		var result = JSON.stringify({
+			userContext: userContext
+		});
+		res.type('application/json').status(200).send(result);
+	});
+
+	app.get('/userProfile', (req, res) => {
+		var userContext = req.authInfo;
+		var result = JSON.stringify({
+			userContext: userContext
+		});
+		res.type('application/json').status(200).send(result);
+	});
+
+	app.get('/configuration', (req, res) => {
+		let options = {};
+		options = Object.assign(options, xsenv.getServices({
+			api: {
+				name: "PARTS_ORDERING_APIM_CUPS"
+			}
+		}));
+		res.json(options);
+	});
+	
+	return app;
+};
