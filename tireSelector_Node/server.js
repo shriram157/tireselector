c0ffbc7a97dcf711f5dbf4765011a0a0ff8d48cb
Anthony Wat <anthony_wat@toyota.ca>
2019-03-07 16:12:04 -0500

    Standardized logging with @sap/logging

--- tireSelector_Node/server.js
+++ tireSelector_Node/server.js
@@ -1,33 +1,49 @@
-/*eslint no-console: 0, no-unused-vars: 0, no-undef:0*/
-/*eslint-env node, es6 */
-'use strict';
-var https = require('https');
+/*eslint new-cap: 0, no-console: 0, no-shadow: 0, no-unused-vars: 0*/
+/*eslint-env es6, node*/
+
+"use strict";
+
+var cors = require("cors");
+var express = require("express");
+var https = require("https");
+var logging = require("@sap/logging");
+var passport = require("passport");
+var xsenv = require("@sap/xsenv");
+var xssec = require("@sap/xssec");
+
+var server = require("http").createServer();
 var port = process.env.PORT || 3000;
-var xsenv = require('@sap/xsenv');
-var server = require('http').createServer();
-https.globalAgent.options.ca = xsenv.loadCertificates();
-global.__base = __dirname + '/';
-//Initialize Express App for XSA UAA and HDBEXT Middleware
-var passport = require('passport');
-var xssec = require('@sap/xssec');
-var xsHDBConn = require('@sap/hdbext');
-var express = require('express');
-//logging
-var logging = require('@sap/logging');
-var appContext = logging.createAppContext();
-//Initialize Express App for XS UAA and HDBEXT Middleware
+
+// Initialize Express app and set up middleware
 var app = express();
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // TODO: 
-app.use(logging.expressMiddleware(appContext));
-//passport and security staff 
-var xsUaaServices = xsenv.getServices({ uaa: { tag: "xsuaa" } });
-passport.use('JWT', new xssec.JWTStrategy(xsUaaServices.uaa));
+
+// Logging
+var appContext = logging.createAppContext();
+app.use(logging.middleware({
+	appContext: appContext,
+	logNetwork: true
+}));
+var logger = appContext.createLogContext().getLogger("/Application/Server");
+
+// XSUAA
+passport.use("JWT", new xssec.JWTStrategy(xsenv.getServices({
+	uaa: {
+		tag: "xsuaa"
+	}
+}).uaa));
 app.use(passport.initialize());
-app.use(passport.authenticate('JWT', { session: false }));
-//Setup Routes
-var router = require('./myRouter')(app, server);
-//Start the Server
-server.on('request', app);
+app.use(passport.authenticate("JWT", {
+	session: false
+}));
+
+// CORS
+app.use(cors());
+
+// Router
+var router = require("./router")(app, appContext);
+
+// Start server
+server.on("request", app);
 server.listen(port, function () {
-	console.info(`HTTP Server: ${server.address().port}`);
+	logger.info("Server is listening on port %d", port);
 });
