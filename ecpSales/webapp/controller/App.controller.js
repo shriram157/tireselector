f09cfb13cfb5e396cb2ae832cf034a2c99156c83
S0019470890 <minakshi_singh@toyota.ca>
2019-03-22 16:07:42 +0000

    changes done for logo.

--- ecpSales/webapp/controller/App.controller.js
+++ ecpSales/webapp/controller/App.controller.js
@@ -4,6 +4,32 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("zecp.controller.App", {
+		onInit: function () {
 
+			var isDivisionSent = window.location.search.match(/Division=([^&]*)/i);
+			var oLexusLogo = this.getView().byId("idLexusLogo");
+			var oToyotaLogo = this.getView().byId("idToyotaLogo");
+			if (isDivisionSent) {
+				this.sDivision = window.location.search.match(/Division=([^&]*)/i)[1];
+
+				if (this.sDivision == "10") // set the Toyoto logo
+				{
+					oToyotaLogo.setProperty("visible", true);
+					oLexusLogo.setProperty("visible", false);
+				} else if (this.sDivision == "20") // set the Lexus logo
+				{
+					oToyotaLogo.setProperty("visible", false);
+					oLexusLogo.setProperty("visible", true);
+				} else { // set the lexus logo
+					oToyotaLogo.setProperty("visible", true);
+					oLexusLogo.setProperty("visible", true);
+				}
+
+			} else {
+				oToyotaLogo.setProperty("visible", true);
+				oLexusLogo.setProperty("visible", true);
+			}
+
+		}
 	});
 });
