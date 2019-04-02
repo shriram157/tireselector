511acee95214351d5866d2787401af04d30ec240
Anthony Wat <anthony_wat@toyota.ca>
2019-04-02 15:28:38 -0400

    Replaced hardcoded UPS name from Node.js code w/ env var from mta.yaml

--- tireSelector_Node/router/routes/app-data.js
+++ tireSelector_Node/router/routes/app-data.js
@@ -18,10 +18,13 @@ module.exports = function (appContext) {
 
 	// TODO Remove this API if it is not used
 	router.get("/configuration", (req, res) => {
+		// Get UPS name from env var UPS_NAME
+		var apimServiceName = process.env.UPS_NAME;
+
 		let options = {};
 		options = Object.assign(options, xsenv.getServices({
 			api: {
-				name: "TIRE_SELECTOR_APIM_CUPS"
+				name: apimServiceName
 			}
 		}));
 		res.json(options);
