b97157bfeeb9f4a17a43b8071f74f472e83c3c53
Aarti Dhamat <aarti_dhamat@toyota.ca>
2019-04-10 10:37:51 +0000

    committing code for Mobile number format Defect 10819

--- tireSelector/webapp/controller/tireQuotation.controller.js
+++ tireSelector/webapp/controller/tireQuotation.controller.js
@@ -142,7 +142,7 @@ sap.ui.define([
 				var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
 				var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
 				if (match) {
-					return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+					return match[1] + '-' + match[2] + '-' + match[3]
 				}
 				return null
 			}
