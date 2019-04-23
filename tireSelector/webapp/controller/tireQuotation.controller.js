0568fa2cb7994adc82eaa654609ea9416f3fb756
Aarti Dhamat <aarti_dhamat@toyota.ca>
2019-04-23 18:03:18 +0000

    commit-pdf fix

--- tireSelector/webapp/controller/tireQuotation.controller.js
+++ tireSelector/webapp/controller/tireQuotation.controller.js
@@ -728,7 +728,7 @@ sap.ui.define([
 				}
 			};
 			
-			if(this.obj.d.VehicleDes == "undefined " || "undefined"){
+			if(this.obj.d.VehicleDes == "undefined " || this.obj.d.VehicleDes == "undefined"){
 				this.obj.d.VehicleDes="";
 			}
 
