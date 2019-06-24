e1976e5855361b59a67dc304b180f67dc44fcae1
Minakshi Singh <minakshi_singh@toyota.ca>
2019-06-24 13:46:32 +0000

    changes done for perform cic and message.

--- ecpSales/webapp/controller/newECPApp.controller.js
+++ ecpSales/webapp/controller/newECPApp.controller.js
@@ -2665,6 +2665,43 @@ sap.ui.define([
 			return linkAddress;
 		},
 		performCIC: function () {
+			var oBusinessModel = this.getModel("ApiBusinessModel");
+			oBusinessModel.read("/A_BusinessPartnerAddress", {
+				urlParameters: {
+					"$filter": "BusinessPartner eq '" + this.getModel("LocalDataModel").getProperty("/VehicleDetails/EndCustomer") +
+						"' ",
+					"$expand": "to_PhoneNumber,to_FaxNumber,to_EmailAddress"
+				},
+				success: $.proxy(function (businessA) {
+					this.getModel("LocalDataModel").setProperty("/OwnerData", businessA.results[0]);
+					if (businessA.results != "") {
+						if (businessA.results[0].to_EmailAddress.results.length > 0) {
+							this.getModel("LocalDataModel").setProperty("/OwnerData/EmailAddress", businessA.results[0].to_EmailAddress.results[
+								0].EmailAddress);
+						} else {
+							this.getModel("LocalDataModel").setProperty("/OwnerData/EmailAddress", "");
+						}
+
+						if (businessA.results[0].to_PhoneNumber.results.length > 0) {
+							this.getModel("LocalDataModel").setProperty("/OwnerData/PhoneNumber", businessA.results[0].to_PhoneNumber.results[
+								0].PhoneNumber);
+						} else {
+							this.getModel("LocalDataModel").setProperty("/OwnerData/PhoneNumber", "");
+
+						}
+						if (businessA.results[0].to_FaxNumber.results.length > 0) {
+							this.getModel("LocalDataModel").setProperty("/OwnerData/FaxNumber", businessA.results[0].to_FaxNumber.results[
+								0].FaxNumber);
+						} else {
+							this.getModel("LocalDataModel").setProperty("/OwnerData/FaxNumber", "");
+
+						}
+					}
+				}, this),
+				error: function () {
+					console.log("Error");
+				}
+			});
 
 			var dealerCode = this.getModel("LocalDataModel").getProperty("/currentIssueDealer");
 			if (!this.oECPData) {
