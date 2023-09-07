071a23807bafadc773ecccd9f00df943c1059adc
Parth Budhiraja <parth_budhiraja@toyota.ca>
2023-09-07 17:16:33 +0000

    Reverting the DB artifact comment and updating Server.js file by adding "tags".

--- tireSelector_XSJS/server.js
+++ tireSelector_XSJS/server.js
@@ -5,17 +5,22 @@ var async_xsjs = require('@sap/async-xsjs');
 
 var port = process.env.PORT || 3000;
 var options = xsenv.getServices({
-    uaa: 'xsuaa',
-    hana: 'hana-hdi',
-    secureStore: 'secureStore'
-    
+	hana: {
+		tag: 'hana-hdi',
+		plan: 'hana-hdi'
+	},
+	uaa: {
+		tag: 'xsuaa'
+	},
+	secureStore: 'secureStore'
+
 });
-async_xsjs(options).then((async_xsjs_server)=>{
-  async_xsjs_server.listen(port, (err)=>{
-    if(!err) {
-      console.log('Node XS server listening on port %d', port);
-    }else{
-      console.log('Node XS server failed to start on port %d', port);
-    }
-  });
+async_xsjs(options).then((async_xsjs_server) => {
+	async_xsjs_server.listen(port, (err) => {
+		if (!err) {
+			console.log('Node XS server listening on port %d', port);
+		} else {
+			console.log('Node XS server failed to start on port %d', port);
+		}
+	});
 });
