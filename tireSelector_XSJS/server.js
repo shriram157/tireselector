fb2772c87b40d4dad2b36aa8fa5a3b726b27b64b
Parth Budhiraja <parth_budhiraja@toyota.ca>
2023-09-07 18:59:53 +0000

    Adding changes in server.js with regards to async-xsjs

--- tireSelector_XSJS/server.js
+++ tireSelector_XSJS/server.js
@@ -5,24 +5,31 @@ var async_xsjs = require('@sap/async-xsjs');
 
 var port = process.env.PORT || 3000;
 var options = {};
+
 // configure HANA
 try {
-	options = Object.assign(options, xsenv.getServices({
-		hana: {
-			tag: "hana"
-		}
-	}));
-	try {
-		options = Object.assign(options, xsenv.getServices({
-			uaa: {
-				tag: "xsuaa"
-			}
-		}));
+    options = Object.assign(options, xsenv.getServices({
+        hana: {
+            tag: "hana"
+        }
+    }));
 
-	} catch (err) {
-		console.log("[WARN]", err.message);
-	}
-});
+} catch (err) {
+    console.log("[WARN]", err.message);
+}
+
+// configure UAA
+try {
+    options = Object.assign(options, xsenv.getServices({
+        uaa: {
+            tag: "xsuaa"
+        }
+    }));
+} catch (err) {
+    console.log("[WARN]", err.message);
+}
+
+
 async_xsjs(options).then((async_xsjs_server) => {
 	async_xsjs_server.listen(port, (err) => {
 		if (!err) {
